379cef98c3335e4d2ab154187295a23f1f56a41f
Vasvary Botond <botyware@gmail.com>
2023-12-18 17:18:30 +0100

    continues migration from svelte and refacts

--- src/app/pages/manage-quizzes-page/DTO.ts
+++ src/app/pages/manage-quizzes-page/DTO.ts
@@ -6,7 +6,7 @@ export class QuizDTO {
     public isOrdered: boolean;
     public timeLimit: number;
 
-    constructor(columnNameItBelongsTo="", indexInColumn=-1, title="<<question>>", quizQuestions=[], isOrdered=false, timeLimit=0) {
+    constructor(columnNameItBelongsTo:string="", indexInColumn:number=-1, title:string="<<question>>", quizQuestions:QuizQuestionDTO[] = [], isOrdered:boolean=false, timeLimit:number=0) {
         this.title = title;
         this.indexInColumn = indexInColumn;
         this.columnNameItBelongsTo = columnNameItBelongsTo;
@@ -22,7 +22,7 @@ export class QuizQuestionDTO {
     public answerIndecies: number[];
     public isOrdered: boolean;
 
-    constructor(questionText="<<question>>", options=["<<option>>", "<<option>>"], answerIndecies=[], isOrdered=false) {
+    constructor(questionText:string="<<question>>", options:string[]=["<<option>>", "<<option>>"], answerIndecies:number[]=[], isOrdered:boolean=false) {
         this.questionText = questionText;
         this.options = options;
         this.answerIndecies = answerIndecies;
--- src/app/pages/manage-quizzes-page/manage-quizzes-page.component.ts
+++ src/app/pages/manage-quizzes-page/manage-quizzes-page.component.ts
@@ -1,14 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injectable, OnInit, WritableSignal, signal } from '@angular/core';
 import { QuizColumnComponent } from "./quiz-column/quiz-column.component";
 import { QuizEditorComponent } from "./quiz-editor/quiz-editor.component";
 import { QuizInstanceEditorComponent } from "./quiz-instance-editor/quiz-instance-editor.component";
-import { QuizDTO } from './DTO';
+import { QuizDTO, QuizQuestionDTO } from './DTO';
 
+@Injectable({ providedIn: 'root' })
 class QuizService {
-  public readQuizzesFromDataStore(): void {console.log("readQuizzesFromDataStore");}
-  public createQuiz(quiz: QuizDTO): void {}
-  public updateQuiz(quiz: QuizDTO): void {}
-  public deleteQuiz(quiz: QuizDTO): void {}
+  // fields, properties
+  //--------------------------------------------------------------
+  public quizzesSig: WritableSignal<QuizDTO[]> = signal([]);
+
+  // constructors
+  //--------------------------------------------------------------
+  constructor() { }
+
+  // methods
+  //--------------------------------------------------------------
+  public readQuizzes() {
+    this.quizzesSig.set(this.readQuizzesInDS());
+  }
+
+  public upsertQuiz(quiz: QuizDTO): void {
+    // optimistic approach
+    // update UI
+    const alreadyExists:boolean = this.quizzesSig().includes(quiz);
+    if (alreadyExists) {
+      this.quizzesSig.update((quizzes) => { return quizzes.map((q) => { return q == quiz ? quiz : q }) }); // or splice but map is more performant this.quizzesSig.update((quizzes) => { return quizzes.splice(quizzes.indexOf(quiz), 1, quiz) });
+    }
+    else if (!alreadyExists) {
+      this.quizzesSig.update((quizzes) => { return [...quizzes, quiz] });
+    }
+    // update data store
+  }
+
+  public deleteQuiz(quiz: QuizDTO): void {
+    // optimistic approach
+    // update UI
+    this.quizzesSig.update((quizzes) => { return quizzes.filter((q) => { return q != quiz }) }); // or splice but filter is more performant this.quizzesSig.update((quizzes) => { return quizzes.splice(quizzes.indexOf(quiz), 1) });
+    // update data store
+  }
+
+  private readQuizzesInDS(): QuizDTO[] {
+    //fake implementation
+    return [
+      new QuizDTO("test", 2, "test1", [new QuizQuestionDTO("test?", ["test1", "test2", "test3"], [])], false, 600),
+      new QuizDTO("test", 2, "test2", [new QuizQuestionDTO("test?", ["test1", "test2", "test3"], [])], false, 600),
+      new QuizDTO("test", 2, "test3", [new QuizQuestionDTO("test?", ["test1", "test2", "test3"], [])], false, 600),
+    ];
+  }
+  private upsertQuizInDS(quiz: QuizDTO): void { }
+  private deleteQuizInDS(quiz: QuizDTO): void { }
 }
 
 @Component({
@@ -31,7 +72,7 @@ export class ManageQuizzesPageComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.quizService.readQuizzesFromDataStore()
+    this.quizService.readQuizzes()
   }
 
 }
