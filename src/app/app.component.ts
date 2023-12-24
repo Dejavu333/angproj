a16bfa48a83ddf8a4bfa1211cc210f26279ee70d
Vasvary Botond <botyware@gmail.com>
2023-12-24 16:45:27 +0100

    continues migration from svelte and refacts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "./shared-components/navbar/navbar.component";
+import { SidebarComponent } from "./shared-components/sidebar/sidebar.component";
 import { DragndropComponent } from "./dragndrop/dragndrop.component";
 
 @Component ({
@@ -8,7 +8,7 @@ import { DragndropComponent } from "./dragndrop/dragndrop.component";
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, NavbarComponent, DragndropComponent],
+    imports: [RouterOutlet, SidebarComponent, DragndropComponent],
 })
 export class AppComponent {
 
--- src/app/shared-components/sidebar/sidebar.component.ts
+++ src/app/shared-components/sidebar/sidebar.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-navbar',
+  selector: 'app-sidebar',
   standalone: true,
   imports: [RouterModule],
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css'
 })
-export class NavbarComponent {
+export class SidebarComponent {
 
 }
