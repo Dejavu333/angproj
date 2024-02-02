cf5ca4d396397d0c2a6e48616b7b63dba4feb17f
Botond Vasvári <botyware@gmail.com>
2024-02-02 18:30:01 +0100

    tool cursors

--- src/app/pages/manage-quizzes-page/quiz-editor/QuizEditorTools.ts
+++ src/app/pages/manage-quizzes-page/quiz-editor/QuizEditorTools.ts
@@ -1,28 +1,38 @@
 export abstract class QuizEditorTool {
     abstract cursor: string;
-    abstract activate():void;
-    abstract clickCommand():void;
+    abstract activate(): void;
+    abstract clickCommand(): void;
 }
 
 export class GroupingTool extends QuizEditorTool {
-    override activate(): void {
-        const territory = document.querySelector("#container") as HTMLElement;
-        territory.style.cursor = this.cursor;
-    }
     override cursor: string = "pointer";
+
+    override activate(): void {
+        const allElements = document.querySelectorAll('*');
+        allElements.forEach((element) => {
+            const el = element as HTMLElement;
+            el.style.setProperty('cursor', this.cursor, 'important');
+        });
+    }
+
     override clickCommand(): void {
         console.log("clickwithgroupingtool");
     }
 }
 
 export class DefaultTool extends QuizEditorTool {
-    override activate(): void {
-        const territory = document.querySelector("#container") as HTMLElement;
-        territory.style.cursor = this.cursor;
-    }
     override cursor: string = "auto";
+
+    override activate(): void {
+        const allElements = document.querySelectorAll('*');
+        allElements.forEach((element) => {
+            const el = element as HTMLElement;
+            el.style.setProperty('cursor', this.cursor, 'important');
+        });
+    }
+
     override clickCommand(): void {
-        console.log("nothing happens since its default tool");    
+        console.log("nothing happens since its default tool");
     }
 }
 
